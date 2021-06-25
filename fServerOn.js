d1fd605b2c69439746e4538f88189595f8d825ab
miragayamx <miragaya.m@gmail.com>
2021-06-24 21:15:07 -0300

    public

--- fServerOn.js
+++ fServerOn.js
@@ -53,7 +53,7 @@ app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 app.use(compression());
 
 app.use('/', loginRouter);
