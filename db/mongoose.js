28a0d865042621b5cfbf8a589941d13f12a5f96b
miragayamx <miragaya.m@gmail.com>
2021-06-24 21:19:26 -0300

    mongo fix

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect("mongodb+srv://6gbGGDEVnKim6bCp:6gbGGDEVnKim6bCp@cluster0.p4bd1.mongodb.net/ecommerce?retryWrites=true&w=majority", {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false,
