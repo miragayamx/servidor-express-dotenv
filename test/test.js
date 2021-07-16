0458418acd54333fe96ae300d99f92e4c616d0c8
miragayamx <miragaya.m@gmail.com>
2021-07-15 21:38:16 -0300

    mocha attach

--- test/test.js
+++ test/test.js
@@ -1,59 +1,59 @@
-const request = require("supertest")("http://localhost:8080/api/productos");
-const expect = require("chai").expect;
-const generador = require("./util/producto");
+const request = require('supertest')('http://localhost:8080/api/productos');
+const expect = require('chai').expect;
+const generador = require('./util/producto');
 
-describe("test productos", () => {
-  describe("GET", () => {
-    it("deberia retornar un status 200", async () => {
-      const response = await request.get('/listar');
-      expect(response.status).to.eql(200);
-    });
-  });
+describe('test productos', () => {
+	describe('GET', () => {
+		it('deberia retornar un status 200', async () => {
+			const response = await request.get('/listar');
+			expect(response.status).to.eql(200);
+		});
+	});
 
-  const producto = generador.get();
-console.log(producto)
-  describe("POST", () => {
-    it("deberia incorporar un producto", async () => {
-      const response = await request.post('/guardar').send(producto);
-      expect(response.status).to.eql(201);
+	const producto = generador.get();
 
-      const item = response.body;
-      expect(item).to.include.keys("title", "price", "thumbnail");
-      expect(item.title).to.eql(producto.title);
-      expect(item.price).to.eql(producto.price);
-    });
-  });
+	describe('POST', () => {
+		it('deberia incorporar un producto', async () => {
+			const response = await request
+				.post('/guardar')
+				.field('title', producto.title)
+				.field('price', producto.price)
+				.attach('thumbnail', producto.thumbnail);
+			expect(response.status).to.eql(201);
 
-  const newProducto = generador.get();
+			const item = response.body;
+			expect(item).to.include.keys('title', 'price', 'thumbnail');
+			expect(item.title).to.eql(producto.title);
+			expect(item.price).to.eql(producto.price);
+		});
+	});
 
-  describe("PUT", () => {
-    it("deberia actualizar un producto", async () => {
-      const getProducts = await request.get('/listar');
-      const findProduct = getProducts.filter(
-        (el) => el.title === producto.title
-      );
-      const response = await request
-        .put(`/actualizar/${findProduct[0]._id}`)
-        .send(newProducto);
-      expect(response.status).to.eql(200);
+	const newProducto = generador.get();
 
-      const item = response.body;
-      expect(item).to.include.keys("title", "price", "thumbnail");
-      expect(item.title).to.eql(newProducto.title);
-      expect(item.price).to.eql(newProducto.price);
-    });
-  });
+	describe('PUT', () => {
+		it('deberia actualizar un producto', async () => {
+			const getProducts = await request.get('/listar');
+			const findProduct = getProducts.body.filter((el) => el.title === producto.title);
+			const response = await request
+				.put(`/actualizar/${findProduct[0]._id}`)
+				.field('title', newProducto.title)
+				.field('price', newProducto.price)
+				.attach('thumbnail', newProducto.thumbnail);
+			expect(response.status).to.eql(200);
 
-  describe("Delete", () => {
-    it("deberia borrar un producto", async () => {
-      const getProducts = await request.get('/listar');
-      const findProduct = getProducts.filter(
-        (el) => el.title === producto.title
-      );
-      const response = await request.delete(
-        `/borrar/${findProduct[0]._id}`
-      );
-      expect(response.status).to.eql(200);
-    });
-  });
+			const item = response.body;
+			expect(item).to.include.keys('title', 'price', 'thumbnail');
+			expect(item.title).to.eql(newProducto.title);
+			expect(item.price).to.eql(newProducto.price);
+		});
+	});
+
+	describe('Delete', () => {
+		it('deberia borrar un producto', async () => {
+			const getProducts = await request.get('/listar');
+			const findProduct = getProducts.body.filter((el) => el.title === newProducto.title);
+			const response = await request.delete(`/borrar/${findProduct[0]._id}`);
+			expect(response.status).to.eql(200);
+		});
+	});
 });
--- test/util/producto.js
+++ test/util/producto.js
@@ -1,11 +1,14 @@
-const faker = require("faker");
+const path = require('path');
+const faker = require('faker');
+
+const imageFile = path.join(__dirname, '../assets/BK-EXAGON-GREEN-1024x1024.png');
 
 const get = () => ({
-  title: faker.commerce.productName(),
-  price: faker.commerce.price(),
-  thumbnail: faker.image.technics(),
+	title: faker.commerce.productName(),
+	price: Number(faker.commerce.price()),
+	thumbnail: imageFile
 });
 
 module.exports = {
-  get,
+	get
 };
